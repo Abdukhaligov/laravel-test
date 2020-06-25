7274824a0ee4e681e8eb5ebb13c4ddc8eaa49c64
Abdukhaligov <hikmat.pou@gmail.com>
2020-06-25 23:29:01 +0400

    Authentication Vue

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
 
 Route::get('/', function () {
   return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
