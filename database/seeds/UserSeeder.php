e7cde26d054648a72750256fe1400faf96814fac
Abdukhaligov <hikmat.pou@gmail.com>
2020-06-26 00:24:05 +0400

    Admin Middleware

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -7,6 +7,7 @@ class UserSeeder extends Seeder {
   public function run() {
     $users = [
         ["name" => "admin", "email" => "admin@site.com", "password" => bcrypt(123456), "admin" => true],
+        ["name" => "user", "email" => "user@site.com", "password" => bcrypt(123456), "admin" => false],
     ];
 
     DB::table('users')->insert($users);
