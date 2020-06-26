7663efe94de854e30f569cfc5f04500a691e12bc
Abdukhaligov <hikmat.pou@gmail.com>
2020-06-26 23:48:34 +0400

    User Registration & validation via UserRequest

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,40 +3,34 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Mail\WelcomeMail;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller {
-  use RegistersUsers;
-
   protected $redirectTo = RouteServiceProvider::HOME;
 
-  public function __construct() {
-    $this->middleware('guest');
+  public function redirectPath() {
+    if (method_exists($this, 'redirectTo')) {
+      return $this->redirectTo();
+    }
+    return property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
   }
 
-  protected function validator(array $data) {
-    return Validator::make($data, [
-        'name' => ['required', 'string', 'max:255'],
-        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-        'password' => ['required', 'string', 'min:8', 'confirmed'],
-    ]);
+  public function showRegistrationForm() {
+    return view('auth.register');
   }
 
-  protected function create(array $data) {
-    $user = User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => Hash::make($data['password']),
-    ]);
+  public function register(UserRequest $request) {
+    $user = User::create($request->all());
 
     Mail::to($user->email)->send(new WelcomeMail($user));
 
-    return $user;
+    Auth::guard()->login($user);
+
+    return redirect($this->redirectPath());
   }
 }
