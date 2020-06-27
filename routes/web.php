2959014fcb2b4705da8653369bfa5df1cf9deaa8
Abdukhaligov <hikmat.pou@gmail.com>
2020-06-28 01:39:09 +0400

    Add Vue Spa & Integrate with Nova

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Auth::routes();
+Route::get('/{any}', 'SpaController@index')->where('any', '^(?!nova).*$');
 
-Route::get('/', function () {
-  return view('welcome');
-});
-
-Route::resource('users','UserController');
-Route::resource('companies','CompanyController');
-Route::resource('positions','PositionController');
-
-Route::group(['middleware' => ['auth']], function () {
-  Route::get('/home', 'HomeController@index')->name('home');
-  Route::get('/profile', 'UserController@profile')->name('profile');
-  Route::patch('/profile', 'UserController@update')->name('userUpdate');
-  Route::get('/profile-media', 'UserController@profileMedia')->name('profileMedia');
-  Route::post('/profile-media', 'UserController@profileMediaUpload');
-});
+//Auth::routes();
+//
+//Route::get('/', function () {
+//  return view('welcome');
+//});
+//
+//Route::resource('users','UserController');
+//Route::resource('companies','CompanyController');
+//Route::resource('positions','PositionController');
+//
+//Route::group(['middleware' => ['auth']], function () {
+//  Route::get('/home', 'HomeController@index')->name('home');
+//  Route::get('/profile', 'UserController@profile')->name('profile');
+//  Route::patch('/profile', 'UserController@update')->name('userUpdate');
+//  Route::get('/profile-media', 'UserController@profileMedia')->name('profileMedia');
+//  Route::post('/profile-media', 'UserController@profileMediaUpload');
+//});
