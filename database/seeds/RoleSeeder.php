0587d494f0552c474be8dacd1ad19bdadeb6aa9d
Abdukhaligov <hikmat.pou@gmail.com>
2020-07-04 15:42:25 +0400

    Update RoleSeeder.php

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\DB;
 class RoleSeeder extends Seeder {
   public function run() {
     (new App\Models\Role(['name'=>'Administrator']))->save();
+    (new App\Models\Role(['name'=>'Moderator']))->save();
+    (new App\Models\Role(['name'=>'Guest']))->save();
 
     DB::table('user_role')->insert(["user_id" => 1, "role_id" => 1]);
   }
